9ab728e8273e78ac973f9c2c76437da6087de5d5
eamederos <eamederos@gmail.com>
2020-05-31 22:17:54 +0200

    Video 20, working with generator yield from

--- generators.py
+++ generators.py
@@ -1,31 +1,10 @@
-print("First exercise")
+
+def get_cites(*cities):
+    for elements in cities:
+       # for subelement in elements:
+        yield from elements
 
 
-def even_number(limit):
-    num = 1
-    my_list = []
-    while num < limit:
-        my_list.append(num*2)
-        num = num+1
-    return my_list
-
-
-print(even_number(10))
-
-print("Second exercise with generator \n")
-
-
-def even_number_generator(limit):
-    num = 1
-    while num < limit:
-        yield num*2
-        num = num+1
-
-
-result = even_number_generator(10)
+result = get_cites("Madrid", "Barcelona", "Valencia", "Aragon")
 print(next(result))
-print("We could have more code here")
 print(next(result))
-print("We could have more code here")
-print(next(result))
-print("We could have more code here")
