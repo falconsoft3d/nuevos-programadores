9f7215406c10f2e762746156b4ebbd10ac85d9a1
eamederos <eamederos@gmail.com>
2020-05-31 23:40:53 +0200

    Video 28, first exercise working with inheritance incapsulating functions or methods

--- inheritance.py
+++ inheritance.py
@@ -7,16 +7,32 @@ class Car():
         self.__on = False
 
     def start(self, decision):
-        self.on = decision
+        self.__on = decision
 
-        if (self.on):
+        if (self.__on):
+            checked = self.__interal_check()
+
+        if (self.__on and checked):
             return "The car is on"
+        elif (self.__on and checked == False):
+            return "Something went wrong during internal check"
         else:
             return "The car is off"
 
     def status(self):
         print("The car has ", self.__wheels, " wheels. It's width is ", self.__chassis_width,
               " and it's lenght is about ", self.__chassis_lenght)
+
+    def __interal_check(self):
+        print("Checking all features")
+        self.fuel = "ok"
+        self.oil = "ok"
+        self.doors = "close"
+
+        if self.fuel == "ok" and self.oil == "ok" and self.doors == "close":
+            return True
+        else:
+            return False
 
 
 my_car = Car()
