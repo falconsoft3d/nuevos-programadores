02e419810f7a3ab277a1a0eb3c4fe9fb49627c31
eamederos <eamederos@gmail.com>
2020-05-31 23:24:40 +0200

    Video 26, first exercise working with inheritance incapsulating attributes

--- inheritance.py
+++ inheritance.py
@@ -1,23 +1,30 @@
 
 class Car():
-    chassis_lenght = 250
-    chassis_width = 120
-    wheels = 4
-    on = False
+    def __init__(self):
+        self.__chassis_lenght = 250
+        self.__chassis_width = 120
+        self.__wheels = 4
+        self.__on = False
 
-    def start(self):
-        self.on = True
+    def start(self, decision):
+        self.on = decision
 
-    def status(self):
-        if self.on:
+        if (self.on):
             return "The car is on"
         else:
             return "The car is off"
 
+    def status(self):
+        print("The car has ", self.__wheels, " wheels. It's width is ", self.__chassis_width,
+              " and it's lenght is about ", self.__chassis_lenght)
+
+
 my_car = Car()
-
-print("The lenght of the car is: " + str(my_car.chassis_lenght))
-print("The car has: " + str(my_car.wheels) + " wheels")
-
-my_car.start()
+print(my_car.start(True))
 print(my_car.status())
+
+print("-----------------Second car will be here-------------------")
+
+second_car = Car()
+print(second_car.start(False))
+print(second_car.status())
