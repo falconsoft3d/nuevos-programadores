114dcb15e81a2a57144a2a2a66df3f7bdcf6b552
eamederos <eamederos@gmail.com>
2020-06-01 00:13:43 +0200

    Video 30, starting with multiple inheritance

--- inheritance.py
+++ inheritance.py
@@ -4,7 +4,7 @@ class Vehicle():
         self.make = pmake
         self.model = pmodel
         self.on = False
-        self.acelerate = False
+        self.accelerate = False
         self.brakes = False
 
     def start(self):
@@ -17,12 +17,59 @@ class Vehicle():
         self.brakes = True
 
     def status(self):
-        print("Make ", self.make, "\nModel ", self.model)
+        print("Make ", self.make, "\nModel ", self.model, "\nOn ", self.on, "\nAceclerate ", self.accelerate,
+              "\nBrakes ", self.brakes)
+
+# ----------------------------------- Class Van -------------------------------------------------------
+
+
+class Van(Vehicle):
+    def to_load(self, pload):
+        self.load = pload
+        if self.load:
+            return "The van is loaded"
+        else:
+            return "The van is not loaded"
+
+# ----------------------------------- Class Moto -------------------------------------------------------
 
 
 class Moto(Vehicle):
+    jump = ""
+    def to_jump(self):
+        self.jump = "This moto is jumping"
+
+    def status(self):
+        print("Make ", self.make, "\nModel ", self.model, "\nOn ", self.on, "\nAceclerate ", self.accelerate,
+              "\nBrakes ", self.brakes, "\nJump ", self.jump)
+
+
+# ----------------------------------- Class Electric Vehicle -------------------------------------------------------
+
+
+class VElectric():
+    def __init__(self):
+        self.autonomy = True
+
+    def charge(self):
+        self.charging = True
+
+
+print("\nMoto features")
+my_moto = Moto("Jawa", "350")
+my_moto.to_jump()
+print(my_moto.status())
+
+print("Van features")
+my_van = Van("Opel", "Corsa")
+my_van.start()
+my_van.status()
+print(my_van.to_load(True))
+
+
+# ----------------------------------- Class EBike -------------------------------------------------------
+
+class Ebike(VElectric, Vehicle):
     pass
 
-
-my_moto = Moto("Jawa", "350")
-print(my_moto.status())
+my_bike = Ebike()
