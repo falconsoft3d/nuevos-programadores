38275f69a41d501106d0047f16dc8b3564d77c41
eamederos <eamederos@gmail.com>
2020-05-31 22:42:36 +0200

    Video 22, working with exceptions including finally syntax

--- exceptions.py
+++ exceptions.py
@@ -19,8 +19,15 @@ def divide(num1, num2):
         return "Wrong operation"
 
 
-op1 = int(input("Type the first number please:"))
-op2 = int(input("Type the second number please:"))
+while True:
+    try:
+        op1 = int(input("Type the first number please:"))
+        op2 = int(input("Type the second number please:"))
+        break;
+    except ValueError:
+        print("You must type a number")
+
+
 operation = input("Type the kind of operation that you want: (add, subtraction, multiply, divide)")
 
 if operation == "add":
@@ -33,3 +40,22 @@ else:
         print(divide(op1, op2))
 
 print("Operation done!!")
+
+
+print("Second exercise \n")
+
+def other_divide():
+    try:
+        op1 = int(input("Type the first number please:"))
+        op2 = int(input("Type the second number please:"))
+        print("Result is: " + str(op2/op2))
+
+    except ValueError:
+        print("You must type a number")
+    except ZeroDivisionError:
+        print("You can not divide by zero")
+    finally:
+        print("Calculation was done!!!")
+
+
+other_divide()
