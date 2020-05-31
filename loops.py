41c98090224ab3134f01e49230449d732a0584de
eamederos <eamederos@gmail.com>
2020-05-31 22:03:50 +0200

    Video 18, working with for else

--- loops.py
+++ loops.py
@@ -1,30 +1,24 @@
-import math
+print("First exercise ")
 
-age = int(input("First exercise: Type your age"))
-
-while age < 5 or age > 100:
-    print("Wrong age. Please type again")
-    age = int(input("Type your age"))
-
-print("Thanks for your collaboration. You are allow to come in")
-
-print("\n")
-print("Second exercise\n")
-
+name = "Informatics Pills"
 count = 0
-print("Square root calculation")
-number = int(input("Type a number"))
 
-while number < 0:
-    print("Negative number can not be evaluated")
-    if count == 2:
-        print("You have tried so many times. Application will terminate")
+for i in name:
+    if i == " ":
+        continue
+    count = count + 1
+
+print("The number of digits is " + str(count))
+
+print("Second exercise \n")
+
+email = input("Type your email, please")
+
+for i in email:
+    if i=="@":
+        is_at = True
         break;
+else:
+    is_at = False
 
-    number = int(input("Type a number again please:"))
-    if count < 0:
-        count = count + 1
-
-if count < 2:
-    solution = math.sqrt(number)
-    print("Square root of number " + str(number) + " is " + str(solution))
+print(is_at)
